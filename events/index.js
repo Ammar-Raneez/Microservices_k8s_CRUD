dbfe8c6f6011e78b71dcfac1458176f4b4274df5
Ammar-Raneez <ammarraneez@gmail.com>
2024-03-10 15:12:02 +0530

    feat: wire up query service with client

--- events/index.js
+++ events/index.js
@@ -5,13 +5,13 @@ import axios from 'axios';
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
 
     try {
-        axios.post('http://localhost:4000/events', event);
-        axios.post('http://localhost:4001/events', event);
-        // axios.post('http://localhost:4002/events', event);
+        await axios.post('http://localhost:4000/events', event);
+        await axios.post('http://localhost:4001/events', event);
+        await axios.post('http://localhost:4002/events', event);
         res.send({ status: 'OK' });
     } catch (err) {
         console.log(err.message);
