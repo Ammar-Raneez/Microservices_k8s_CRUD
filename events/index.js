a070aa335de25a96206820b7dc39c1e98407f24e
Ammar-Raneez <ammarraneez@gmail.com>
2024-08-16 13:55:24 +0530

    Wire up posts and event bus

--- events/index.js
+++ events/index.js
@@ -1,35 +1,36 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
 
 const events = [];
 
-app.post('/events', async (req, res) => {
-    const event = req.body;
-    events.push(event);
+app.post("/events", async (req, res) => {
+  const event = req.body;
+  events.push(event);
 
-    try {
-        await axios.post('http://localhost:4000/events', event);
-        await axios.post('http://localhost:4001/events', event);
-        await axios.post('http://localhost:4002/events', event);
-        await axios.post('http://localhost:4003/events', event);
+  try {
+    // Posts ClusterIP name
+    await axios.post("http://posts-clusterip-srv:4000/events", event);
+    //await axios.post('http://localhost:4001/events', event);
+    //await axios.post('http://localhost:4002/events', event);
+    //await axios.post('http://localhost:4003/events', event);
 
-        console.log("Events fired");
-        res.send({ status: 'OK' });
-    } catch (err) {
-        console.log(err.message);
-        res.status(502).send({ error: err.message });
-    }
+    console.log("Events fired");
+    res.send({ status: "OK" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(502).send({ error: err.message });
+  }
 });
 
-app.get('/events', (req, res) => {
-    console.log(events);
-    res.send(events);
+app.get("/events", (req, res) => {
+  console.log(events);
+  res.send(events);
 });
 
 app.listen(4005, () => {
-    console.log('Listening on port 4005');
+  console.log("Listening on port 4005");
 });
--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { randomBytes } from 'crypto';
-import cors from 'cors';
-import axios from 'axios';
+import express from "express";
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,37 +10,39 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = { id, title };
+  posts[id] = { id, title };
 
-    try {
-        await axios.post('http://localhost:4005/events', {
-            type: 'PostCreated',
-            data: {
-                id, title
-            }
-        });
+  try {
+    // Event Bus ClusterIP name
+    await axios.post("http://event-bus-srv:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    });
 
-        res.status(201).send(posts[id]);
-    } catch (err) {
-        console.log(err.message);
-        res.status(502).send({ error: err.message });
-    }
+    res.status(201).send(posts[id]);
+  } catch (err) {
+    console.log(err.message);
+    res.status(502).send({ error: err.message });
+  }
 });
 
-app.post('/events', (req, res) => {
-    console.log('Received event', req.body.type);
-    res.send({});
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log(`Latest update ${new Date().getTime()}`);
-    console.log('Listening on port 4000');
+  console.log(`Latest update ${new Date().getTime()}`);
+  console.log("Listening on port 4000");
 });
